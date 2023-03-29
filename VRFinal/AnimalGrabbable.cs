676e727e1263ea76241c223ed3d965bec33034f3
jordan <54329371+jordansprojects@users.noreply.github.com>
2023-03-29 13:29:03 -0700

    Add files via upload

--- VRFinal/AnimalGrabbable.cs
+++ VRFinal/AnimalGrabbable.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 using BNG;
 using EmeraldAI;
 
-public class AnimalGrabbable : MonoBehaviour
-{
-   // public editor variables 
-    public bool isGrabbed;
 
-    // This is important for rotating the animal properly 
-    // when it is being picked up and released 
-    public Transform animalTransform;	
-    // Set to the animals animator, so that it can be disabled
+
+public class AnimalGrabbable : BNG.GrabbableEvents
+{
+
+   // Set to the animals animator, so that it can be disabled
     // when an animal is picked up, and renabled when the animal is put down
     public Animator animator; 
 
@@ -35,25 +32,7 @@ public class AnimalGrabbable : MonoBehaviour
 	navmesh.velocity = transform.forward * navmesh.speed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-	// This is just for debugging, we do not want
-	// this function in update() !!
-	checkForGrab();
-
-    }
-
-    
-    public void checkForGrab(){
-    	if(isGrabbed == true){
-		onGrab();
-	}else{
-		onDrop();
-	}
-    }
-
-    public void onGrab(){
+    override public void OnGrab(Grabber grabber){
     	//disable animation controller
 	animator.enabled = false;
 	//Stop navmesh agent 
@@ -61,7 +40,7 @@ public class AnimalGrabbable : MonoBehaviour
 	
     }
 
-    public void onDrop(){
+    override public void OnRelease(){
     	// Renable animation controller
 	animator.enabled = true;
  	//Restart navmesh agent
